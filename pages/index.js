5997c891dfbcf859ce8ae8923112370189afbe4e
Viencab <vaccabrera.student@ua.edu.ph>
2024-02-15 12:20:46 +0800

    second commit

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+    10 things that require Zero Talent</p>
+<ul>
+<li>1. Being On Time</li>
+    <li> 1. Being On Time. </li>
+    <li> 2. Making An Effort. </li>
+    <li> 3. Being High Energy. </li>
+    <li> 4. Having A Positive Attitude. </li>
+    <li> 5. Being Passionate </li>
+    <li> 6. Using Good Body Language </li>
+    <li> 7. Being Coachable </li>
+    <li> 8. Doing A Little Extra </li>
+    <li> 9. Being Prepared. </li>
+    <li> 10. Having A Strong Work Ethic </li>  
+</ul>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
